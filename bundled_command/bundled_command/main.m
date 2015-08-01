88a511d8da7d514af487e917730cf3f87e0dae72
1024jp <1024jp@wolfrosch.com>
2015-08-01 16:16:53 +0900

    Implement command-line tool

--- bundled_command/bundled_command/main.m
+++ bundled_command/bundled_command/main.m
@@ -7,11 +7,32 @@
 //
 
 @import Foundation;
+#import "CommandLineBundleSample.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        switch (argc) {
+            case 1:
+                printf("Hello, pass me a file path.\n");
+                return 0;
+            case 2:
+                break;
+            default:
+                printf("The command can take just one argument.\n");
+                return 1;
+        }
+        
+        // make URL object from argument
+        NSURL *url = [[NSURL fileURLWithFileSystemRepresentation:argv[1] isDirectory:NO relativeToURL:nil] URLByStandardizingPath];
+        
+        printf("absolute path: %s/n", [url fileSystemRepresentation]);
+        
+        // create scriptable application object
+        CommandLineBundleSampleApplication *application = [SBApplication applicationWithBundleIdentifier:@"com.wolfrosch.CommandLineBundleSample"];
+        
+        // let application open the given file
+        [application open:@[url]];
+        [application activate];
     }
     return 0;
 }
