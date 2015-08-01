e5b7c823c3548dad33690842327425a9bfde97e6
1024jp <1024jp@wolfrosch.com>
2015-08-01 16:01:23 +0900

    Create command-line project

---
+++ bundled_command/bundled_command/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  bundled_command
+//
+//  Created by 1024jp on 8/1/15.
+//
+//
+
+@import Foundation;
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
